5c7b779991eed89c28d720bffe3bc93985a96c85
Odion <olumhenseo@gmail.com>
2022-10-16 14:32:48 +0000

    combo

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -9,49 +9,25 @@
  */
 int main(void)
 {
-        int a, b, c;
-
-       /** for (d = 1; d < 100; d++)
-        {
-                if (d % 10 == 0 || d % 11 == 0 || d % 111 == 0 || ((int)(d % 10 * 10) + d / 10) < d)
-                        continue;
-                e = d / 10;
-		f = d / 10;
-                putchar(e + '0');
-		putchar(f + '0');
-                putchar(d % 10 + '0');
-
-                if (d != 89)
-                {
-                        putchar(',');
-                        putchar(' ');
-                }
-        }*/
+	int a, b, d;
 
 	for (a = 0; a <= 7; a++)
 	{
-		for (b = 1; b <= 8; b++ )
+		for (b = 1; b <= 8; b++)
 		{
-			for (c = 2; c <= 9; c++)
+			for (d = 2; d <= 9; d++)
 			{
-			//	if (abc % 10 == 0)
-			//		continue;
 				putchar(a + '0');
 				putchar(b + '0');
-				putchar(c + '0');
-		
-
-				if (a != 7 && b != 8 && c != 9)
-                		putchar(',');
+				putchar(d + '0');
+				if (a != 7 || b != 8 || d != 9)
+				putchar(',');
 				putchar(' ');
 			}
 		}
 	}
 
+	putchar('\n');
 
-
-
-        putchar('\n');
-
-        return (0);
+	return (0);
 }
