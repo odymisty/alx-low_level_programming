daf3db47583accce2fbdea7e243447c47659d44a
Odion <olumhenseo@gmail.com>
2022-10-16 11:28:45 +0000

    print conbination

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -13,13 +13,13 @@ int main(void)
 
 	for (d = 1; d < 100; d++)
 	{
-		if (d % 10 == 0 || d % 11 == 0 || ((int)(d % 10 * 10) + d / 10) < d )
+		if (d % 10 == 0 || d % 11 == 0 || ((int)(d % 10 * 10) + d / 10) < d)
 			continue;
-		e = d / 10;
-		putchar(e + '0');
-		putchar(d % 10 + '0');
+			e = d / 10;
+			putchar(e + '0');
+			putchar(d % 10 + '0');
 
-		if (d != 99)
+		if (d != 89)
 		{
 			putchar(',');
 			putchar(' ');
