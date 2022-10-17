79fb4f2a1e91a4c12f31d52e3f065ee5ed687e80
Odion <olumhenseo@gmail.com>
2022-10-17 16:42:19 +0000

    twoCombo

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -9,38 +9,38 @@
  */
 int main(void)
 {
-	int d, e, f, g, h, i;
+int d, e, f, g, h, i;
 
-	for (d = 0; d <= 9; d++)
+for (d = 0; d <= 9; d++)
+{
+	for (e = 0; e <= 9; e++)
 	{
-		for (e = 0; e <= 9; e++)
+		for (f = 0; f <= 9; f++)
 		{
-			for (f = 0; f <= 9; f++)
+			for (g = 0; g <= 9; g++)
 			{
-				for (g = 0; g <= 9; g++)
+				h = (d * 10) + e;
+				i = (f * 10) + g;
+
+				if (h < i)
 				{
-					h = (d * 10) + e;
-					i = (f * 10) + g;
+					putchar(d + '0');
+					putchar(e + '0');
+					putchar(' ');
+					putchar(f + '0');
+					putchar(g + '0');
 
-					if (h < i)
+					if (d == 9 && e == 8 && f == 9 && g == 9)
+						break;
 					{
-						putchar(d + '0');
-						putchar(e + '0');
+						putchar(',');
 						putchar(' ');
-						putchar(f + '0');
-						putchar(g + '0');;
-
-						if (d == 9 && e == 8 && f == 9 && g == 9)
-							break;
-						{
-							putchar(',');
-							putchar(' ');
-						}
 					}
 				}
 			}
 		}
 	}
+}
 
 	putchar('\n');
 
