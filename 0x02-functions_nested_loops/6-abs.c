f072bde3288e67395d5a0da44889961ebe10bae8
Odion <olumhenseo@gmail.com>
2022-10-18 16:23:51 +0000

    absolute

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _abs - compute absolute value
+ *
+ * @x: given value
+ *
+ * Return: Absolute value of integer
+ */
+int _abs(int x)
+{
+	if (x >= 0)
+	{
+		return (x);
+	}
+	else
+	{
+		return (-x);
+	}
+}
