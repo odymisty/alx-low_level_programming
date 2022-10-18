720dc24f0ae5f29a780e549a5680e5aa7b0f75b7
Odion <olumhenseo@gmail.com>
2022-10-18 15:42:28 +0000

    lower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _islower - prints the alphabet in lower cases
+ *
+ * Return: 1 if lowercase and 0 if not
+ */
+
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
