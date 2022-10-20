60b125b9053ec59400686ff63315d3e5b05c2fad
Odion <olumhenseo@gmail.com>
2022-10-20 19:49:21 +0000

    Digits

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -11,9 +11,8 @@ void print_most_numbers(void)
 
 	for (i = '0'; i <= '9'; i++)
 	{
-		if (i == 2 || i == 4)
-			continue;
-		_putchar(i);
+		if (i != 2 && i != 4)
+			_putchar(i);
 	}
 	_putchar('\n');
 }
