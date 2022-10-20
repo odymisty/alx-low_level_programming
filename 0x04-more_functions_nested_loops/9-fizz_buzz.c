2d18605f21a882399bc01c3fd5272a4be3fa4c63
Odion <olumhenseo@gmail.com>
2022-10-20 23:00:24 +0000

    FizzBuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+ * main - prints numbers with fizz buzz
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 1; a <= 100; a++)
+	{
+		if (a < 100)
+			printf(" ");
+
+		if (a % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (a % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else if (a % 3 == 0 && a % 5 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else
+		{
+			printf("%d ", a);
+		}
+	}
+
+	printf("\n");
+
+	return (0);
+}
