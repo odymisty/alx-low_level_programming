54a72951e87af2215ad4f80ace940890bac96420
Odion <olumhenseo@gmail.com>
2022-10-24 20:51:50 +0000

    Pointers

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _puts - prints a string followed by a newline
+ * @str: the pointer to the str
+ *
+ * Return: void
+ */
+void _puts(char *str)
+{
+	puts(str);
+}
