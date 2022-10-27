e27b3a55e4448e66f81a7441c662848dde5538b1
Odion <olumhenseo@gmail.com>
2022-10-27 12:33:49 +0000

    Concatenated two strings

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,17 +1,21 @@
-#include <string.h>
 #include "main.h"
 
 /**
- * _strncpy - copies a string
- * @dest: the string to be copied into
- * @src: the string to be copied
- * @n: the number of bytes
+ * _strncat - Concatenates two strings using at most
+ *            an inputted number of bytes from src.
+ * @dest: The string to be appended upon.
+ * @src: The string to be appended to dest.
+ * @n: The number of bytes from src to be appended to dest.
  *
- * Return: a pointer to dest
+ * Return: A pointer to the resulting string dest.
  */
-char *_strncpy(char *dest, char *src, int n)
+char *_strncat(char *dest, char *src, int n)
 {
-	char *val = strncpy(dest, src, n);
+	int index = 0, dest_len = 0;
 
-	return (val);
+	while (dest[index++])
+		dest_len++;
+	for (index = 0; src[index] && index < n; index++)
+		dest[dest_len++] = src[index];
+	return (dest);
 }
