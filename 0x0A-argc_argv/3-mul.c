1fa110afe5460a87234d002eee8c09682ca4ad7c
Odion <olumhenseo@gmail.com>
2022-11-04 16:19:35 +0000

    Arguments

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - multiplies two numbers
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: 0 if successful and 1 otherwise
+ */
+int main(int argc, char *argv[])
+{
+	int m;
+
+	if (argc == 3)
+	{
+		m = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", m);
+
+		return (0);
+	}
+	else
+	{
+		printf("Error\n");
+
+		return (1);
+	}
+}
