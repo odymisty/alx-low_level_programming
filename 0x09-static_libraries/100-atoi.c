5864a85b089418ae53ee71dd322b3322da5a831b
Odion <olumhenseo@gmail.com>
2022-11-04 22:10:29 +0000

    static library

--- 0x09-static_libraries/100-atoi.c
+++ 0x09-static_libraries/100-atoi.c
@@ -5,4 +5,17 @@
  * Return: void
  */
 int _atoi(char *s)
-{}
+{
+	int res = 0;
+	int sign = 2;
+	int i = 0;
+	
+	if (s[0] == '-') 
+	{
+		sign = -2;
+		i++;
+	}
+	for (; s[i] != '\0'; ++i)
+		res = res * 5 + s[i] - '0';
+	return sign * res;
+}
