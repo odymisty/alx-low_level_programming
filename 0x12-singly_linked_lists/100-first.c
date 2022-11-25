c270fc39ef1d149fad4c0b005a46de707cad20e3
Odion <olumhenseo@gmail.com>
2022-11-25 22:14:36 +0000

    New function

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+/**
+ * before_main -prints a sentence before makn executes
+ *
+ * Return: void
+ */
+void before_main(void)
+{
+	printf("You're beat! and yet, you must allow,\n"
+			"I bore my house upon my back!\n");
+}
